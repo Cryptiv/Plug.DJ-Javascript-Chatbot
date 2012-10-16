1ba40661db6811e57c9a126f32b3a5c2ac0c255b
Cryptiv <quicktime115@gmail.com>
2012-10-16 15:08:03 -0400

    Facebook

--- bin/bot.coffee
+++ bin/bot.coffee
@@ -1025,15 +1025,15 @@ class fbCommand extends Command
 
 	functionality: ->
 		m = Math.floor Math.random()*@msgs.length
-		msg = @msgs[m].replace('{fb}','http://on.fb.me/HNzK5S')
+		msg = @msgs[m].replace('{fb}','http://www.facebook.com/1ND1EPLUS')
 		API.sendChat msg
 
 	msgs: [
 		"Don't have any friends in real life? That's ok, we'll be your friend.  Join our facebook group: {fb}",
-		"Wondering what TIMarbury looks like?  Join our facebook group ({fb}) and find out for yourself!",
+		"Wondering what a d a m looks like?  Join our facebook group ({fb}) and find out for yourself!",
 		"We have a facebook group. Join it. Please. {fb}",
-		"The Dubstep Den is now on friendster! lol just kidding.  Here's our facebook group: {fb} you should join.",
-		"I bet you're handsome.  Join our facebook group so me0w can stalk your photos: {fb}"
+		"INDIE+ is now on friendster! lol just kidding.  Here's our facebook page: {fb} you should join.",
+		"I bet you're handsome.  Join our facebook group so Nora can stalk your photos: {fb}"
 	]
 		
 
--- bin/bot.js
+++ bin/bot.js
@@ -1648,11 +1648,11 @@
     fbCommand.prototype.functionality = function() {
       var m, msg;
       m = Math.floor(Math.random() * this.msgs.length);
-      msg = this.msgs[m].replace('{fb}', 'http://on.fb.me/HNzK5S');
+      msg = this.msgs[m].replace('{fb}', 'http://www.facebook.com/1ND1EPLUS');
       return API.sendChat(msg);
     };
 
-    fbCommand.prototype.msgs = ["Don't have any friends in real life? That's ok, we'll be your friend.  Join our facebook group: {fb}", "Wondering what TIMarbury looks like?  Join our facebook group ({fb}) and find out for yourself!", "We have a facebook group. Join it. Please. {fb}", "The Dubstep Den is now on friendster! lol just kidding.  Here's our facebook group: {fb} you should join.", "I bet you're handsome.  Join our facebook group so me0w can stalk your photos: {fb}"];
+    fbCommand.prototype.msgs = ["Don't have any friends in real life? That's ok, we'll be your friend.  Join our facebook group: {fb}", "Wondering what a d a m looks like?  Join our facebook group ({fb}) and find out for yourself!", "We have a facebook group. Join it. Please. {fb}", "INDIE+ is now on friendster! lol just kidding.  Here's our facebook page: {fb} you should join.", "I bet you're handsome.  Join our facebook group so Nora can stalk your photos: {fb}"];
 
     return fbCommand;
 
